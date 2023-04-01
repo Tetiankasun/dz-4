38dec80f9ac955d1771015b1eb2933215378d6cc
yaroslavTt <yaroslav.tykhovetskyi@gmail.com>
2023-04-01 11:03:59 +0300

    Added required functionality

---
+++ src/main/java/org/example/HomeTask.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class HomeTask {
+    int x = 10;
+
+    public int printMessage() {
+        System.out.println("---" + x + "---");
+        return this.x;
+    }
+
+}
